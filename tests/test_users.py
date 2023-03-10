f80987180b34322943cb62a8b08468b8bb65dd59
TamirlanMakhov <makhov.tamirlan@gmail.com>
2023-03-10 17:21:03 +0300

    testing user creation and response code

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
@@ -9,3 +10,11 @@ def test_root():
     response = client.get('/')
     assert response.json() == {"message": "welcome to main page"}
     assert response.status_code == 200
+
+
+def test_create_user():
+    response = client.post('users', json={"email": "test@mail.ru", "password": "123"})
+    new_user = schemas.UserResponse(**response.json())
+
+    assert new_user.email == 'test@mail.ru'
+    assert response.status_code == 201
