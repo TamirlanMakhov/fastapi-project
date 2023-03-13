6b522362f97b8b772dd49a62845982986a0d737b
TamirlanMakhov <makhov.tamirlan@gmail.com>
2023-03-13 16:15:09 +0300

    refactor: separate file for db session and fixture

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,37 +1,5 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 from app import schemas
-from app.config import settings
-from app.database import get_db, Base
-from app.main import app
-
-import pytest
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@' \
-                          f'{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-@pytest.fixture
-def client():
-    Base.metadata.create_all(bind=engine)  # create tables
-    Base.metadata.drop_all(bind=engine)  # drop tables, so there will be not duplicates every time we run tests
-    yield TestClient(app)  # implements tests as a fixture in functions
+from .database import client, session
 
 
 def test_root(client):
