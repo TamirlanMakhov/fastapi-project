d401ef2c72c88e8ae239c43321f877fe0f42d697
TamirlanMakhov <makhov.tamirlan@gmail.com>
2023-03-13 15:51:24 +0300

    added fixture to create new table before each test and then drop the table

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,19 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.main import app
+
 from app import schemas
 from app.config import settings
 from app.database import get_db, Base
+from app.main import app
+
+import pytest
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@' \
                           f'{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
 
 
 def override_get_db():
@@ -25,16 +26,21 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-client = TestClient(app)
+
+@pytest.fixture
+def client():
+    Base.metadata.create_all(bind=engine)  # create tables
+    yield TestClient(app)  # implements tests as a fixture in functions
+    Base.metadata.drop_all(bind=engine)  # drop tables, so there will be not duplicates every time we run tests
 
 
-def test_root():
+def test_root(client):
     response = client.get('/')
     assert response.json() == {"message": "welcome to main page"}
     assert response.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
     response = client.post('users', json={"email": "test@mail.ru", "password": "123"})
     new_user = schemas.UserResponse(**response.json())
 
