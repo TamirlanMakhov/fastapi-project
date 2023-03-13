8116022a1c4765e6235dcd24773a6981f77bde84
TamirlanMakhov <makhov.tamirlan@gmail.com>
2023-03-13 18:05:21 +0300

    granular user creation function

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,20 @@
+import pytest
+
 from app import schemas
 from .database import client, session
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "test@mail.ru", "password": "123"}
+    response = client.post('/users/', json=user_data)
+
+    assert response.status_code == 201
+
+    new_user = response.json()
+    new_user['password'] = user_data['password']
+
+    return new_user
 
 
 def test_root(client):
@@ -16,6 +31,6 @@ def test_create_user(client):
     assert response.status_code == 201
 
 
-def test_login_user(client):
-    response = client.post('/login', data={"username": "test@mail.ru", "password": "123"})
+def test_login_user(test_user, client):
+    response = client.post('/login', data={"username": test_user['email'], "password": test_user['password']})
     assert response.status_code == 200
