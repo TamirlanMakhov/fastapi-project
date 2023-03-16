16aa4f243d079fdf0493a7d4a47d6b68a2141685
TamirlanMakhov <makhov.tamirlan@gmail.com>
2023-03-16 20:52:52 +0300

    deleted hardcoded values for raw-sql connection

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@' \
+                          f'{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -22,7 +23,10 @@ def get_db():
 # raw-sql
 # while True:
 #     try:
-#         conn = psycopg2.connect(host='localhost', database='fastapidb', user='vinc', password='350053ZOz')
+#         conn = psycopg2.connect(host='localhost',
+    #         database=settings.database_name,
+    #         user=settings.database_username,
+    #         password=settings.database_password)
 #         cursor = conn.cursor()
 #         print('success')
 #         break
